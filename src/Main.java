fb26c1eaa0686ba487379eb9cfaefc5e976650a4
XarlosZakk <carlosgamer1009@gmail.com>
2025-05-03 19:52:05 -0500

    primer repositorio de recordatorio de java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String nombre = "carlos";
+        String apellido = "serrano";
+        System.out.println(nombre);
+    }
+}
